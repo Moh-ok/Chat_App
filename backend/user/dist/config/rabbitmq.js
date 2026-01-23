4c1904d0b1ab2c512a05dc538123cc5b6dd2f404
MohanK-pixel <mohnk007@gmail.com>
2026-01-23 12:53:53 +0530

    feat: add user and mail services(redis and rabbitmq implimentation)
    initial
    setup

---
+++ backend/user/dist/config/rabbitmq.js
@@ -0,0 +1,29 @@
+import amqp from 'amqplib';
+let channel;
+export const connectRabbitHQ = async () => {
+    try {
+        const connection = await amqp.connect({
+            protocol: "amqp",
+            hostname: process.env.Rabbitmq_Host,
+            port: 5672,
+            username: process.env.Rabbitmg_Username,
+            password: process.env.Rabbitmg_Password,
+        });
+        channel = await connection.createChannel();
+        console.log("✅Connected to rabbitmq");
+    }
+    catch (error) {
+        console.log("Failed to connect to rabbitma", error);
+    }
+};
+export const publishToQueue = async (queueName, message) => {
+    if (!channel) {
+        console.log("Rabbitmq channel is not initalized");
+        return;
+    }
+    await channel.assertQueue(queueName, { durable: true });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+    });
+};
+//# sourceMappingURL=rabbitmq.js.map
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -2,9 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv'
 import connectDb from './config/db.js';
 import { createClient } from 'redis';
+import userRoutes from './routes/user.js'
+import { connectRabbitHQ } from './config/rabbitmq.js';
+
 dotenv.config();
 
 connectDb();
+
+connectRabbitHQ();
 
 export const redisClient = createClient({
     url: process.env.REDIS_URL!,
@@ -12,10 +17,12 @@ export const redisClient = createClient({
 
 redisClient
     .connect()
-    .then(() => console.log("connected to redis"))
+    .then(() => console.log("Connected to redis"))
     .catch(console.error);
 
-const app = express()
+const app = express();
+
+app.use("api/v1", userRoutes);
 
 const port = process.env.PORT;
 
