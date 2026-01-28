557a35e13b2905f08c1bddd8916a5ee82ba55efb
MohanK-pixel <mohnk007@gmail.com>
2026-01-28 20:15:23 +0530

    feat: chat service implimentation
    chat service setup with mongodb

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -17,6 +17,7 @@ redisClient
     .catch(console.error);
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1", userRoutes);
 const port = process.env.PORT;
 app.listen(port, () => {
