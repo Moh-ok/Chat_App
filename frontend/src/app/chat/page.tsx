7d7966d517872c6c980cf2d1396924da19841b17
MohanK-pixel <mohnk007@gmail.com>
2026-02-11 16:27:28 +0530

    Chat header

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 import ChatSidebar from '@/components/ChatSidebar';
 import Loading from '@/components/Loading'
-import { useAppData, User } from '@/context/AppContext'
+import { chat_service, useAppData, User } from '@/context/AppContext'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import ChatHeader from '@/components/ChatHeader';
 
 export interface Message {
   _id: string;
@@ -43,6 +47,53 @@ const ChatApp = () => {
 
   const handleLogout = () => logoutUser();
 
+  async function fetchChat() {
+    const token = Cookies.get("token");
+    try {
+      const { data } = await axios.get(`${chat_service}/api/v1/message/${selectedUser}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    setMessages(data.messages);
+    setUser(data.use);
+    await fetchChats();
+    } catch (error) {
+      toast.error("Failed to load messages");
+    }
+  }
+
+  async function createChat(u: User) {
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.post(`${chat_service}/api/v1/chat/new`,
+      {
+        userId: loggedInUser?._id,
+        otherUserId: u._id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setSelectedUser(data.chatId);
+      setShowAllUser(false);
+      await fetchChats();
+    } catch (error) {
+      toast.error("Failed to start chat");
+    }
+  }
+
+  useEffect(() => {
+    if (selectedUser) {
+      fetchChat();
+    }
+  }, [selectedUser]);
+
   if (loading) return <Loading />
 
   return (
@@ -51,7 +102,18 @@ const ChatApp = () => {
       <ChatSidebar sidebarOpen={siderbarOpen} setSidebarOpen={setSiderbarOpen}
       showAllUsers={showAllUser} setShowAllUsers={setShowAllUser} users={users}
       loggedInUser={loggedInUser} chats={chats} selectedUser={selectedUser}
-      setSelectedUser={setSelectedUser} handleLogout={handleLogout} />
+      setSelectedUser={setSelectedUser} handleLogout={handleLogout}
+      createChat={createChat} 
+    />
+
+    <div className="flex-1 flex flex-col justify-between p-4 backdrop-blur-xl
+    bg-white/5 border-1 border-white/10">
+      <ChatHeader
+        user={user}
+        setSidebarOpen={setSiderbarOpen}
+        isTyping={isTyping}
+      />
+    </div>
     </div>
   )
 }
